6f07f6976ec23fd52945744c5d8e11372ba70fd9
Max <s724959099@yahoo.com.tw>
2023-01-05 09:08:56 +0800

    feat: translate to component

---
+++ src/components/VTranslateion.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row>
+    <v-col cols="6">
+      <v-card>
+        <v-card-title>
+          <h1>English</h1>
+        </v-card-title>
+        <v-card-text>{{ english }}</v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="6">
+      <v-card>
+        <v-card-title>
+          <h1>Translation</h1>
+        </v-card-title>
+        <v-card-text>{{ english }}</v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script lang="ts" setup>
+defineProps<{
+  english: string;
+}>();
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,32 +8,17 @@
         <v-textarea solo name="" label="" v-model="textInput"></v-textarea>
       </v-card-text>
     </v-card>
-    <v-row :key="index" class="my-3" v-for="(item, index) in setntenceList">
-      <v-col cols="6">
-        <v-card>
-          <v-card-title>
-            <h1>English</h1>
-          </v-card-title>
-          <v-card-text>
-            {{ item }}
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="6">
-        <v-card>
-          <v-card-title>
-            <h1>Translation</h1>
-          </v-card-title>
-          <v-card-text>
-            {{ item }}
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-translateion
+      :key="index"
+      class="my-5"
+      :english="item"
+      v-for="(item, index) in setntenceList"
+    />
   </div>
 </template>
 <script lang="ts" setup>
 import { ref, watch, reactive } from 'vue';
+import VTranslateion from '@/components/VTranslateion.vue';
 
 const textInput = ref(
   'The retail dataset contains item clicks from an online apparel store, and browsing logs for the same users, collected during May 2013. The apps dataset contains downloads of Windows Phone 8 Apps and browsing logs for the same users, collected between September and November 2013 (inclusive). These datasets were intentionally selected to represent diverse domains: the retail dataset contains page clicks on pages of physical goods, while the apps dataset contains purchases of virtual multimedia goods. Our experimental results, thus, demonstrate the applicability of our enhancement methods across different types of user-item interaction types as well as varied product catalogs.'
@@ -45,7 +30,7 @@ watch(textInput, (val) => {
   setntenceList = val
     .replace(/^\.+|\.+$/g, '')
     .split('.')
-    .map((item) => item + '.');
+    .map((item) => item.trim() + '.');
   console.log(setntenceList);
 });
 </script>
