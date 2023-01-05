a263b27207b2bda079407b31c0e1f3c39e011448
Max <s724959099@yahoo.com.tw>
2023-01-05 08:24:41 +0800

    feat: add simple ui

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+import { createVuetify } from 'vuetify';
 
-import './assets/main.css';
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/styles';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 const app = createApp(App);
-
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+app.use(vuetify);
 app.use(createPinia());
 app.use(router);
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,51 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue';
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <v-card>
+      <v-card-title>
+        <h1>文章段落</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-textarea solo name="" label="" v-model="textInput"></v-textarea>
+      </v-card-text>
+    </v-card>
+    <v-row :key="index" class="my-3" v-for="(item, index) in setntenceList">
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>
+            <h1>English</h1>
+          </v-card-title>
+          <v-card-text>
+            {{ item }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>
+            <h1>Translation</h1>
+          </v-card-title>
+          <v-card-text>
+            {{ item }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
+<script lang="ts" setup>
+import { ref, watch, reactive } from 'vue';
+
+const textInput = ref(
+  'The retail dataset contains item clicks from an online apparel store, and browsing logs for the same users, collected during May 2013. The apps dataset contains downloads of Windows Phone 8 Apps and browsing logs for the same users, collected between September and November 2013 (inclusive). These datasets were intentionally selected to represent diverse domains: the retail dataset contains page clicks on pages of physical goods, while the apps dataset contains purchases of virtual multimedia goods. Our experimental results, thus, demonstrate the applicability of our enhancement methods across different types of user-item interaction types as well as varied product catalogs.'
+);
+let setntenceList: string[] = reactive([]);
+// watch
+watch(textInput, (val) => {
+  console.log(val);
+  setntenceList = val
+    .replace(/^\.+|\.+$/g, '')
+    .split('.')
+    .map((item) => item + '.');
+  console.log(setntenceList);
+});
+</script>
