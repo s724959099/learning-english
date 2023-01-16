78ba489ed70e836d716bc9ad6efe6658ebed470b
Max <s724959099@yahoo.com.tw>
2023-01-16 22:03:15 +0800

    temp: translate temp

--- src/components/VTranslateion.vue
+++ src/components/VTranslateion.vue
@@ -28,7 +28,38 @@
 </template>
 <script lang="ts" setup>
 import VWord from '@/components/VWord.vue';
-import { computed, reactive } from 'vue';
+import { computed, reactive, ref } from 'vue';
+import { watch } from 'vue';
+import axios from 'axios';
+
+function debounce<F extends (...params: any[]) => void>(fn: F, delay: number) {
+  let timeoutID: number = 0;
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutID);
+    timeoutID = window.setTimeout(() => fn.apply(this, args), delay);
+  } as F;
+}
+
+function translateToChinese(sourceText: string) {
+  let sourceLang = 'en';
+  let targetLang = 'zh-tw';
+
+  let url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURI(
+    sourceText
+  )}`;
+  axios
+    .get(url)
+    .then((res) => {
+      translatedSentence.value = res.data[0][0][0];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// axios.get(url).then((res) => {
+//   console.log(res.data[0][0][0]);
+// });
 
 const props = defineProps<{
   english: string;
@@ -38,15 +69,22 @@ const words = reactive(
     return { word: item, active: true };
   })
 );
-const translatedSentence = computed(() => {
+const translatedSentence = ref('');
+const activeEnglishSentence = computed(() => {
   return words
     .filter((item) => item.active)
     .map((item) => item.word)
     .join(' ');
 });
+const updateWatch = debounce((val) => {
+  translateToChinese(val);
+}, 2000);
+// watch translatedSentence changed
+watch(activeEnglishSentence, (val) => {
+  // updateWatch(val);
+});
 
 function clickWord(index: number) {
   words[index].active = !words[index].active;
-  console.log('clickWord', index);
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,26 +14,27 @@
       :english="item"
       v-for="(item, index) in setntenceList"
     />
-    <v-translateion
-      english="The retail dataset contains item clicks from an online apparel store, and browsing logs for the same users."
-    />
+    {{ setntenceList }}
+    {{ demo }}
   </div>
 </template>
 <script lang="ts" setup>
 import { ref, watch, reactive } from 'vue';
 import VTranslateion from '@/components/VTranslateion.vue';
 
-const textInput = ref(
-  'The retail dataset contains item clicks from an online apparel store, and browsing logs for the same users, collected during May 2013. The apps dataset contains downloads of Windows Phone 8 Apps and browsing logs for the same users, collected between September and November 2013 (inclusive). These datasets were intentionally selected to represent diverse domains: the retail dataset contains page clicks on pages of physical goods, while the apps dataset contains purchases of virtual multimedia goods. Our experimental results, thus, demonstrate the applicability of our enhancement methods across different types of user-item interaction types as well as varied product catalogs.'
-);
-let setntenceList: string[] = reactive(['i am english']);
+const textInput = ref('');
+const demo = ref('');
+
+let setntenceList: string[] = reactive([]);
 // watch
 watch(textInput, (val) => {
+  demo.value = val;
   console.log(val);
   setntenceList = val
     .replace(/^\.+|\.+$/g, '')
     .split('.')
     .map((item) => item.trim() + '.');
+  console.log(setntenceList.length);
   console.log(setntenceList);
 });
 </script>
